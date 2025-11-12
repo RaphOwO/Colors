35be79c2522c5cf25937ba9b6f3afa6c1f728401
Suwitchaya Chintawan <suwitchaya@Suwitchayas-MacBook-Air.local>
2025-11-12 13:09:55 +0700

    Update quiz JSON with new questions and make it's to be randomly

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -5,6 +5,18 @@ import "./../styles/testpage.css";
 import testData from "../content/test.json";
 
 function TestPage({ user }) {
+
+  const shuffleQ = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  const [questions] = useState(() => shuffleQ(testData));
+
   const [current, setCurrent] = useState(0);
   const [score, setScore] = useState(0);
   const [selected, setSelected] = useState(null);
@@ -22,7 +34,7 @@ function TestPage({ user }) {
     );
   }
 
-  const question = testData[current];
+  const question = questions[current];
 
   const handleOptionClick = (option) => {
     if (!submitted) setSelected(option);
@@ -37,7 +49,7 @@ function TestPage({ user }) {
   const handleNext = () => {
     setSubmitted(false);
     setSelected(null);
-    if (current + 1 < testData.length) {
+    if (current + 1 < questions.length) {
       setCurrent((prev) => prev + 1);
     } else {
       setFinished(true);
@@ -59,7 +71,7 @@ function TestPage({ user }) {
           <>
             <h1>Colors Test</h1>
             <h3>
-              Question {current + 1} of {testData.length}
+              Question {current + 1} of {questions.length}
             </h3>
             <p className="question">{question.question}</p>
 
@@ -67,9 +79,9 @@ function TestPage({ user }) {
               {question.options.map((option, index) => {
                 let className = "option-btn";
                 if (submitted) {
-                  if (option === question.answer) className += " correct"; 
-                  else if (option === selected) className += " wrong"; 
-                  else className += " disabled"; 
+                  if (option === question.answer) className += " correct";
+                  else if (option === selected) className += " wrong";
+                  else className += " disabled";
                 } else if (selected === option) className += " selected";
                 return (
                   <button
@@ -88,11 +100,8 @@ function TestPage({ user }) {
               <div className="explain-box">
                 <p>{selected === question.answer ? "Correct!" : "Wrong!"}</p>
                 <p>{question.explanation}</p>
-                <button
-                  className="next-btn"
-                  onClick={handleNext}
-                >
-                  {current + 1 === testData.length ? "Finish" : "Next"}
+                <button className="next-btn" onClick={handleNext}>
+                  {current + 1 === questions.length ? "Finish" : "Next"}
                 </button>
               </div>
             ) : (
@@ -107,10 +116,14 @@ function TestPage({ user }) {
           </>
         ) : (
           <div className="result-box">
-            <img className="color-img" src="https://www.pngmart.com/files/23/Color-Wheel-PNG-Photos.png" alt= "Good image"></img>
+            <img
+              className="color-img"
+              src="https://www.pngmart.com/files/23/Color-Wheel-PNG-Photos.png"
+              alt="Good image"
+            />
             <h2>Test Completed!</h2>
             <p>
-              You scored {score} out of {testData.length}.
+              You scored {score} out of {questions.length}.
             </p>
             <button className="next-btn" onClick={handleRestart}>
               Try Again
